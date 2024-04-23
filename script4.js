f4644ee834742b600e408e09912bad7ee10e666a
prabubalakrishna <164847527+prabubalakrishna@users.noreply.github.com>
2024-04-23 14:14:56 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,10 @@
+Palindrome = (arr, n) =>
+    {
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }
