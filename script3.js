630c806e486703c6a900c30a1a547e58cfb4f084
prabubalakrishna <164847527+prabubalakrishna@users.noreply.github.com>
2024-04-23 14:14:40 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,21 @@
+let n=34;
+let l=(n)=>
+{
+   for(let i=2; i<=n; i++)
+   {
+      let flag=0;
+      for(let j=2; j<i; j++)
+      {
+         if(i%j==0)
+         {
+            flag=1;
+            break;
+         }
+      }
+      if(flag==0)
+      {
+         console.log(i);
+      }
+   }
+}
+console.log(l(n));
