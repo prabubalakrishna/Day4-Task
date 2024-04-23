e93cfedead6ef67cb1ca50cb34ceff52d16fcdb3
prabubalakrishna <164847527+prabubalakrishna@users.noreply.github.com>
2024-04-23 14:14:06 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+var str="my name is ayesha";
+let def= (str)=>{
+   str = str.toLowerCase().split(' ');
+   for (var i = 0; i < str.length; i++) {
+     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+   } 
+   return str.join(' ');
+}
+console.log(def(str));
